7b572e83348df0f561141d08c37ba9047e5e7388
Sneh Koul <snehkoul1999@gmail.com>
2024-08-15 12:57:38 -0400

    remove l3 because its broken

--- .github/workflows/testnode.bash
+++ .github/workflows/testnode.bash
@@ -6,11 +6,10 @@
 cd ${GITHUB_WORKSPACE}
 
 # TODO once develop is merged into nitro-contract's master, remove the NITRO_CONTRACTS_BRANCH env var
-./test-node.bash --init-force --l3node --no-simple --detach
+./test-node.bash --init-force  --no-simple --detach
 
 START=$(date +%s)
 L2_TRANSACTION_SUCCEEDED=false
-L3_TRANSACTION_SUCCEEDED=false
 SUCCEEDED=false
 
 while true; do
@@ -21,14 +20,8 @@ while true; do
         fi
     fi
 
-    if [ "$L3_TRANSACTION_SUCCEEDED" = false ]; then
-        if ${GITHUB_WORKSPACE}/test-node.bash script send-l3 --ethamount 100 --to user_l3user --wait; then
-            echo "Sending l3 transaction succeeded"
-            L3_TRANSACTION_SUCCEEDED=true
-        fi
-    fi
 
-    if [ "$L2_TRANSACTION_SUCCEEDED" = true ] && [ "$L3_TRANSACTION_SUCCEEDED" = true ]; then
+    if [ "$L2_TRANSACTION_SUCCEEDED" = true ]; then
         SUCCEEDED=true
         break
     fi
