b41364dfef60af181ab8c13a5bfe43e4e2ec7390
Sneh Koul <snehkoul1999@gmail.com>
2024-10-22 13:32:39 -0500

    address comments

--- smoke-test-altlayer.bash
+++ smoke-test-altlayer.bash
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-COMPOSE_FILE=docker-compose-altlayer.yaml
 
-./altlayer-test-node.bash --init --latest-espresso-image --detach
+#  Run altlayer config with batch poster, sequencer, full node and validator
+./test-node.bash --init-force --validate --batchposters 1 --latest-espresso-image --detach 
 
 docker compose up -d full-node --detach
 
-
 # Sending L2 transaction through the full-node's api
 user=user_l2user
-./altlayer-test-node.bash script send-l2 --ethamount 100 --to $user --wait
+./test-node.bash script send-l2 --ethamount 100 --to $user --wait
 
 # Check the balance from full-node's api
 userAddress=$(docker compose run scripts print-address --account $user | tail -n 1 | tr -d '\r\n')
