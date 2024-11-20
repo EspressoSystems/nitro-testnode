71956a49c7e7fbd16d85bbab69fcc9b758ca683e
Sneh Koul <snehkoul1999@gmail.com>
2024-11-20 18:22:40 -0500

    merge

--- test-node.bash
+++ test-node.bash
@@ -318,7 +318,7 @@ done
 
 if $espresso; then
     NITRO_CONTRACTS_REPO=https://github.com/EspressoSystems/nitro-contracts.git
-    NITRO_CONTRACTS_BRANCH=develop
+    NITRO_CONTRACTS_BRANCH=update-nitro-scripts
     export NITRO_CONTRACTS_REPO
     export NITRO_CONTRACTS_BRANCH
     echo "Running espresso mode"
