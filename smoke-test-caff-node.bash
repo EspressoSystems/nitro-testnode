bfaaf1d0b02d64d372f934d1ce5cacda9f7f8e41
Jeremy <yiliang.he@qq.com>
2025-02-21 21:14:29 +0800

    Fix caff node test (#85)
    * WIP
    
    * WIP
    
    * small fixes
    
    * rename to caff
    
    * fix
    
    * use bc for big numbers
    
    * Send transaction through caff node
    
    ---------
    
    Co-authored-by: Sneh Koul <snehkoul1999@gmail.com>

--- smoke-test-caff-node.bash
+++ smoke-test-caff-node.bash
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-listen_to_sequencer_feed() {
-    #  Listen to the sequencer feed and check if the sender address is detected
-    while read -r message; do
-        # Check if the message contains the specific sender address
-        if [[ "$message" == *"\"sender\":\"0xdd6bd74674c356345db88c354491c7d3173c6806\""* ]]; then
-            echo "Sender address detected"
-            break
-        fi
-    done < <(wscat -c ws://127.0.0.1:9642)
-}
 
 
-./test-node.bash --espresso --latest-espresso-image --validate --tokenbridge --init-force --detach --espresso-finality-node
+user=user_l2user
+caff_url="ws://caff-node:8548"
+
+./test-node.bash --espresso --latest-espresso-image --validate --tokenbridge --init-force --detach --caff-node
 
 # Start the caff node
 docker compose up -d caff-node --wait --detach
 
-# Sending L2 transaction
-./test-node.bash script send-l2 --ethamount 100 --to user_l2user --wait
+echo "Sending L2 transaction through sequencer"
+./test-node.bash script send-l2 --ethamount 10 --to $user --wait
 
-listen_to_sequencer_feed
+# Check the balance from caff node's api
+userAddress=$(docker compose run scripts print-address --account $user | tail -n 1 | tr -d '\r\n')
 
+while true; do
+    # Check if the balance on Caff node is greater than 0
+    balance=$(cast balance $userAddress --rpc-url http://127.0.0.1:8550)
+    # Using bc here because it supports bigint
+    if [ "$(echo "$balance > 0" | bc)" -eq 1 ]; then
+        break
+    fi
+    sleep 1
+done
+
+sleep 5
+echo "Sending L2 transaction through caff node"
+./test-node.bash script send-l2 --ethamount 10 --to $user --l2url $caff_url --wait
+
+echo "Smoke test succeeded."
 docker compose down
