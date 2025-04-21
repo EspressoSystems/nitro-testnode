38d17904532aeb3ac1eaae2b0df4ee4a8f87c3f6
Jean Gal <jgatbkk@gmail.com>
2025-04-21 11:32:46 -0400

    update nitro node version

--- test-node.bash
+++ test-node.bash
@@ -3,7 +3,7 @@
 set -eu
 
 DEFAULT_NITRO_CONTRACTS_REPO="https://github.com/OffchainLabs/nitro-contracts.git"
-NITRO_NODE_VERSION=offchainlabs/nitro-node:v3.2.1-d81324d-dev
+NITRO_NODE_VERSION=offchainlabs/nitro-node:v3.5.2-33d30c0
 BLOCKSCOUT_VERSION=offchainlabs/blockscout:v1.1.0-0e716c8
 
 # This commit matches v2.1.0 release of nitro-contracts, with additional support to set arb owner through upgrade executor
