301c9b1de86afe37988fc983a1e3b176e3d4b882
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2025-04-22 20:21:57 +0530

    Fix caff node test (#100)
    * Fix caff node test
    
    * comment out l3
    
    * fix launch bash
    
    * fix caff node test
    
    * cleanup
    
    * fix smoke test
    
    * fix migration test
    
    * fix l3 test
    
    * comment out l3 test
    
    * Restart sequencer when it hangs
    
    ---------
    
    Co-authored-by: Jeremy <yiliang.he@qq.com>

--- smoke-test-caff-node.bash
+++ smoke-test-caff-node.bash
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
+check_nonce_increase() {
+    local address=$1
+    local initial_nonce=$2
+    while true; do
+        currentNonce=$(cast nonce "$address" --rpc-url ws://127.0.0.1:8552)
+        if [ "$currentNonce" -eq "$((initial_nonce + 1))" ]; then
+            break
+        fi
+        sleep 1
+    done
+}
 
 user=user_l2user
+funnel=funnel
 caff_url="ws://caff-node:8548"
 
 ./test-node.bash --espresso --latest-espresso-image --validate --tokenbridge --init-force --detach --caff-node
@@ -11,11 +22,12 @@ caff_url="ws://caff-node:8548"
 # Start the caff node
 docker compose up -d caff-node --wait --detach
 
-echo "Sending L2 transaction through sequencer"
+echo "Sending L2 transaction through sequencer 1"
 ./test-node.bash script send-l2 --ethamount 10 --to $user --wait
 
 # Check the balance from caff node's api
 userAddress=$(docker compose run scripts print-address --account $user | tail -n 1 | tr -d '\r\n')
+funnelAddress=$(docker compose run scripts print-address --account $funnel | tail -n 1 | tr -d '\r\n')
 
 while true; do
     # Check if the balance on Caff node is greater than 0
@@ -27,14 +39,24 @@ while true; do
     sleep 1
 done
 
-echo "Sending L2 transaction through caff node"
+nonce=$(cast nonce $funnelAddress --rpc-url ws://127.0.0.1:8552)
+
+echo "Sending L2 transaction through caff node 1"
 ./test-node.bash script send-l2 --ethamount 10 --to $user --l2url $caff_url --wait
 
-echo "Sending L2 transaction through sequencer"
+check_nonce_increase "$funnelAddress" "$nonce"
+nonce=$((nonce + 1))
+
+echo "Sending L2 transaction through sequencer 2"
 ./test-node.bash script send-l2 --ethamount 10 --to $user --wait
 
-echo "Sending L2 transaction through caff node"
+check_nonce_increase "$funnelAddress" "$nonce"
+nonce=$((nonce + 1))
+
+echo "Sending L2 transaction through caff node 2"
 ./test-node.bash script send-l2 --ethamount 10 --to $user --l2url $caff_url --wait
+
+check_nonce_increase "$funnelAddress" "$nonce"
 
 echo "Smoke test succeeded."
 docker compose down
