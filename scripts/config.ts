958ea540a68144329fb7f11bd159dd1f493524fd
Jeremy <yiliang.he@qq.com>
2025-05-21 12:57:41 +0800

    Fix caff node test

--- scripts/config.ts
+++ scripts/config.ts
@@ -302,7 +302,7 @@ function writeConfigs(argv: any) {
 
   if (argv.espresso) {
     let config = baseConfig as any;
-    config.node["batch-poster"]["hotshot-url"] = "";
+    config.node["batch-poster"]["hotshot-urls"] = [];
     config.node["batch-poster"]["light-client-address"] = "";
   }
 
@@ -328,7 +328,7 @@ function writeConfigs(argv: any) {
     }
     if (argv.espresso) {
       simpleConfig.node.feed.output.enable = true;
-      simpleConfig.node["batch-poster"]["hotshot-url"] = argv.espressoUrl;
+      simpleConfig.node["batch-poster"]["hotshot-urls"] = [argv.espressoUrl];
       simpleConfig.node["batch-poster"]["light-client-address"] =
         argv.lightClientAddress;
       simpleConfig.node["block-validator"]["dangerous"][
@@ -417,12 +417,12 @@ function writeConfigs(argv: any) {
       sequencerConfig.node["espresso-caff-node"] = {
         enable: true,
         "hotshot-urls": [argv.espressoUrl],
-        "fallback-urls": [argv.espressoUrl],
         "next-hotshot-block": 1,
         namespace: 412346,
         "hotshot-polling-interval": "250ms",
         "retry-time": "2s",
-        "espresso-tee-verifier-addr": "0xb562622f2D76F355D673560CB88c1dF6088702f1",
+        "legacy-sgx-verifier-addr": "0xb562622f2D76F355D673560CB88c1dF6088702f1",
+        "batch-poster-addr": "0xe2148eE53c0755215Df69b2616E552154EdC584f",
       };
 
       sequencerConfig.execution["forwarding-target"] = "ws://sequencer:8548";
@@ -440,9 +440,11 @@ function writeConfigs(argv: any) {
     let posterConfig = JSON.parse(baseConfJSON);
     if (argv.espresso) {
       posterConfig.node.feed.input.url.push("ws://sequencer:9642");
-      posterConfig.node["batch-poster"]["hotshot-url"] = argv.espressoUrl;
+      posterConfig.node["batch-poster"]["hotshot-urls"] = [argv.espressoUrl];
       posterConfig.node["batch-poster"]["light-client-address"] =
         argv.lightClientAddress;
+      posterConfig.node["batch-poster"]["espresso-tee-verifier-address"] =
+        "0xb562622f2D76F355D673560CB88c1dF6088702f1";
     } else {
       posterConfig.node["seq-coordinator"].enable = true;
     }
