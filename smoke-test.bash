12d93f50b806409be1aff7d00088bbfe5fad7e8a
Jeremy <yiliang.he@qq.com>
2025-07-17 13:44:14 +0800

    Fix smoke test

--- smoke-test.bash
+++ smoke-test.bash
@@ -3,17 +3,23 @@ set -euo pipefail
 
 ./test-node.bash --espresso --latest-espresso-image --validate --tokenbridge --init-force --detach
 
+export http_proxy=""
+export https_proxy=""
+export all_proxy=""
+
+rollupAddress=$(docker compose run --entrypoint sh poster -c "jq -r '.[0].rollup.rollup' /config/deployed_chain_info.json | tail -n 1 | tr -d '\r\n'")
+previousConfirmed=$(cast call --rpc-url http://localhost:8545 $rollupAddress 'latestConfirmed()(bytes32)')
+echo "latest confirmed hash: $previousConfirmed"
+
 # Sending L2 transaction
 ./test-node.bash script send-l2 --ethamount 100 --to user_l2user --wait
 
-rollupAddress=$(docker compose run --entrypoint sh poster -c "jq -r '.[0].rollup.rollup' /config/deployed_chain_info.json | tail -n 1 | tr -d '\r\n'")
 while true; do
-  confirmed=$(cast call --rpc-url http://localhost:8545 $rollupAddress 'latestConfirmed()(uint256)')
-  echo "Number of confirmed staking nodes: $confirmed"
-  if [ "$confirmed" -gt 0 ]; then
+  confirmed=$(cast call --rpc-url http://localhost:8545 $rollupAddress 'latestConfirmed()(bytes32)')
+  if [ -n "$confirmed" ] && [ "$confirmed" != "$previousConfirmed" ]; then
     break
   else
-    echo "Waiting for more confirmed nodes ..."
+    echo "Waiting for more confirmed nodes ...$confirmed"
   fi
   sleep 5
 done
