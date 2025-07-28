4fd1605d6b78c812d0de6f6c8723c333d461ccaf
Jeremy <yiliang.he@qq.com>
2025-07-28 11:48:14 +0800

    Update nitro contract

--- test-node.bash
+++ test-node.bash
@@ -325,7 +325,7 @@ done
 
 if $espresso; then
     NITRO_CONTRACTS_REPO=https://github.com/EspressoSystems/nitro-contracts.git
-    NITRO_CONTRACTS_BRANCH=develop
+    NITRO_CONTRACTS_BRANCH=celestia-v3.1.0
     export NITRO_CONTRACTS_REPO
     export NITRO_CONTRACTS_BRANCH
     echo "Running espresso mode"
