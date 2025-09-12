74efa0b3d827f42e553f3579cb3e372924e60343
Fibonacci747 <albertofibonacci12@gmail.com>
2025-09-12 17:18:54 +0200

    Update test-node.bash

--- test-node.bash
+++ test-node.bash
@@ -15,7 +15,7 @@ DEFAULT_TOKEN_BRIDGE_VERSION="v1.2.2"
 ESPRESSO_DEFAULT_VERSION=ghcr.io/espressosystems/nitro-espresso-integration/nitro-node:integration
 : ${ESPRESSO_VERSION:=$ESPRESSO_DEFAULT_VERSION}
 
-# Set default versions if not overriden by provided env vars
+# Set default versions if not overridden by provided env vars
 : ${NITRO_CONTRACTS_REPO:=$DEFAULT_NITRO_CONTRACTS_REPO}
 : ${NITRO_CONTRACTS_BRANCH:=$DEFAULT_NITRO_CONTRACTS_VERSION}
 : ${TOKEN_BRIDGE_BRANCH:=$DEFAULT_TOKEN_BRIDGE_VERSION}
