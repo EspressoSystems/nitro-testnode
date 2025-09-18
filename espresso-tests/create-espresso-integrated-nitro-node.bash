72166d367885dd916a50117c6c1fb6469a742aba
Jeremy <yiliang.he@qq.com>
2025-09-18 15:42:26 +0800

    Fix espresso nitro image

--- espresso-tests/create-espresso-integrated-nitro-node.bash
+++ espresso-tests/create-espresso-integrated-nitro-node.bash
@@ -2,7 +2,7 @@
 set -euo pipefail
 # set -x # print each command before executing it, for debugging
 
-ESPRESSO_VERSION=ghcr.io/espressosystems/nitro-espresso-integration/nitro-node:jh-reorg-tx-streamer-after-restart
+ESPRESSO_VERSION=ghcr.io/espressosystems/nitro-espresso-integration/nitro-node:integration
 lightClientAddr=0xb6eb235fa509e3206f959761d11e3777e16d0e98
 espresso=true
 simpleWithValidator=false
