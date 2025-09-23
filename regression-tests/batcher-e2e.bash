bb77c77389c25738bbbeb1367bdefccd14b359ca
Jeremy <yiliang.he@qq.com>
2025-09-23 11:32:49 +0800

    Load test

--- regression-tests/batcher-e2e.bash
+++ regression-tests/batcher-e2e.bash
@@ -18,10 +18,10 @@ echo "starting nodes"
 ../test-node.bash --init-force --espresso $(get_espresso_image_flag) --validate --detach
 
 echo "starting tx spammer"
-docker compose run --detach scripts send-l2 --ethamount 10 --to user_l2user --times 2000 --delay 200 --wait
+docker compose run --detach scripts send-l2 --ethamount 1 --to user_l2user --times 10000000 --delay 1000 --wait
 
 echo "sending delayed tx"
-docker compose run --detach scripts send-l2-delayed --ethamount 10 --to user_delayed_user --from espresso-sequencer --times 100 --delay 2000 --wait
+docker compose run --detach scripts send-l2-delayed --ethamount 1 --to user_delayed_user --from espresso-sequencer --times 1000000 --delay 2000 --wait
 
 check_validator_root_matches_sequencer() {
     local block=$1
@@ -34,55 +34,62 @@ check_validator_root_matches_sequencer() {
     echo "validator block number: $block, validator root: $validator_root, sequencer root: $sequencer_root"
 }
 
-sleep 20
-validator_block=0
-i=0
-while [[ $i -lt 5 ]]; do
-    sleep 60
-    new_validator_block=$(cast block-number --rpc-url http://localhost:8247)
-    echo "validator block number: $new_validator_block"
-    if [[ "$new_validator_block" -eq "$validator_block" ]]; then
-        # validator has caught up
-        i=$((i+1))
-        continue
-    fi
-
-    validator_block=$new_validator_block
+for i in {1..1000}; do
+    validator_block=$(cast block-number --rpc-url http://localhost:8247)
     check_validator_root_matches_sequencer $validator_block
+    sleep 600
 done
 
-echo "validator stops creating blocks for the last 60 seconds. validator block number: $validator_block"
-sequencer_block=$(cast block-number --rpc-url http://localhost:8547)
-sequencer_inbox=$(docker compose run --entrypoint sh poster -c "jq -r '.[0].rollup.\"sequencer-inbox\"' /config/deployed_chain_info.json | tail -n 1 | tr -d '\r\n'")
-echo "sequencer inbox: $sequencer_inbox"
-batch_count=$(cast call $sequencer_inbox "batchCount()(uint256)" --rpc-url http://localhost:8545)
-echo "batch count: $batch_count"
 
-if (( sequencer_block > validator_block + batch_count + 1 )); then
-    echo "Error: sequencer block number ($sequencer_block) is greater than validator block number ($validator_block) + batch count ($batch_count)"
-    exit 1
-fi
-echo "validator has caught up. validator block number: $validator_block"
+# sleep 20
+# validator_block=0
+# i=0
+# while [[ $i -lt 5 ]]; do
+#     sleep 60
+#     new_validator_block=$(cast block-number --rpc-url http://localhost:8247)
+#     echo "validator block number: $new_validator_block"
+#     if [[ "$new_validator_block" -eq "$validator_block" ]]; then
+#         # validator has caught up
+#         i=$((i+1))
+#         continue
+#     fi
 
-validator_block=$(cast block-number --rpc-url http://localhost:8247)
-check_validator_root_matches_sequencer $validator_block
+#     validator_block=$new_validator_block
+#     check_validator_root_matches_sequencer $validator_block
+# done
 
-docker compose run scripts update-config-value \
-    --path /config/poster_config.json \
-    --property node.batch-poster.max-empty-batch-delay \
-    --value "30s" \
+# echo "validator stops creating blocks for the last 60 seconds. validator block number: $validator_block"
+# sequencer_block=$(cast block-number --rpc-url http://localhost:8547)
+# sequencer_inbox=$(docker compose run --entrypoint sh poster -c "jq -r '.[0].rollup.\"sequencer-inbox\"' /config/deployed_chain_info.json | tail -n 1 | tr -d '\r\n'")
+# echo "sequencer inbox: $sequencer_inbox"
+# batch_count=$(cast call $sequencer_inbox "batchCount()(uint256)" --rpc-url http://localhost:8545)
+# echo "batch count: $batch_count"
 
-docker compose restart poster
+# if (( sequencer_block > validator_block + batch_count + 1 )); then
+#     echo "Error: sequencer block number ($sequencer_block) is greater than validator block number ($validator_block) + batch count ($batch_count)"
+#     exit 1
+# fi
+# echo "validator has caught up. validator block number: $validator_block"
 
-# Batcher should have created 1 empty batch
-sleep 40
+# validator_block=$(cast block-number --rpc-url http://localhost:8247)
+# check_validator_root_matches_sequencer $validator_block
 
-validator_block2=$(cast block-number --rpc-url http://localhost:8247)
-if (( validator_block2 == validator_block )); then
-    echo "Error: validator block number ($validator_block2) is not greater than validator block number ($validator_block)"
-    exit 1
-fi
+# docker compose run scripts update-config-value \
+#     --path /config/poster_config.json \
+#     --property node.batch-poster.max-empty-batch-delay \
+#     --value "30s" \
 
-check_validator_root_matches_sequencer $validator_block2
+# docker compose restart poster
+
+# # Batcher should have created 1 empty batch
+# sleep 40
+
+# validator_block2=$(cast block-number --rpc-url http://localhost:8247)
+# if (( validator_block2 == validator_block )); then
+#     echo "Error: validator block number ($validator_block2) is not greater than validator block number ($validator_block)"
+#     exit 1
+# fi
+
+# check_validator_root_matches_sequencer $validator_block2
 
 docker compose down --remove-orphans
