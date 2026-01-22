e50c083ae5fc33a610e4f776ee1b15687c435758
Varun Doshi <doshivarun202@gmail.com>
2026-01-22 20:54:44 +0530

    update image version

--- espresso-tests/create-espresso-integrated-nitro-node.bash
+++ espresso-tests/create-espresso-integrated-nitro-node.bash
@@ -2,7 +2,7 @@
 set -euo pipefail
 # set -x # print each command before executing it, for debugging
 
-ESPRESSO_VERSION=ghcr.io/espressosystems/nitro-espresso-integration/nitro-node:v3.8.0-2495bf4
+ESPRESSO_VERSION=ghcr.io/espressosystems/nitro-espresso-integration/nitro-node:v3.9.2-e8d0468
 lightClientAddr=0xb6eb235fa509e3206f959761d11e3777e16d0e98
 simpleWithValidator=false
 
