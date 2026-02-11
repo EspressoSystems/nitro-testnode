30f616e235e4a4bad4775b650a379d2515bb2405
Varun Doshi <doshivarun202@gmail.com>
2026-02-11 11:04:16 +0530

    fix: failing force inclusion test

--- regression-test.bash
+++ regression-test.bash
@@ -13,13 +13,6 @@ scripts=$(find . -maxdepth 1 -name '*.bash' \
 for script in $scripts; do
   name=$(basename "$script")
 
-  for skip in "${SKIP_TESTS[@]}"; do
-    if [[ "$name" == "$skip" ]]; then
-      echo "⚠️  Skipping $name"
-      continue 2
-    fi
-  done
-
   echo "Running $(basename "$script")"
   attempt=1
   max_attempts=3
